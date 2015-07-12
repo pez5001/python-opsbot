c4078b567c6f9a08fae299cb8c2740cbf98ef75d
pez5001 <pez5001@gmail.com>
2015-07-12 14:23:27 -0700

    mainly some formatting and style changes

--- rtmbot.py
+++ rtmbot.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import sys
+
 sys.dont_write_bytecode = True
 
 import glob
 import yaml
-import json
 import os
 import sys
 import time
@@ -14,9 +14,11 @@ from argparse import ArgumentParser
 
 from slackclient import SlackClient
 
+
 def dbg(debug_string):
     if debug:
         logging.info(debug_string)
+
 
 class RtmBot(object):
     def __init__(self, token):
@@ -24,10 +26,12 @@ class RtmBot(object):
         self.token = token
         self.bot_plugins = []
         self.slack_client = None
+
     def connect(self):
         """Convenience method that creates Server instance"""
         self.slack_client = SlackClient(self.token)
         self.slack_client.rtm_connect()
+
     def start(self):
         self.connect()
         self.load_plugins()
@@ -38,19 +42,22 @@ class RtmBot(object):
             self.output()
             self.autoping()
             time.sleep(.1)
+
     def autoping(self):
-        #hardcode the interval to 3 seconds
+        # hardcode the interval to 3 seconds
         now = int(time.time())
         if now > self.last_ping + 3:
             self.slack_client.server.ping()
             self.last_ping = now
+
     def input(self, data):
-        if "type" in data:
-            function_name = "process_" + data["type"]
-            dbg("got {}".format(function_name))
+        if 'type' in data:
+            function_name = 'process_' + data['type']
+            dbg('got {}'.format(function_name))
             for plugin in self.bot_plugins:
                 plugin.register_jobs()
                 plugin.do(function_name, data)
+
     def output(self):
         for plugin in self.bot_plugins:
             limiter = False
@@ -60,68 +67,73 @@ class RtmBot(object):
                     if limiter == True:
                         time.sleep(.1)
                         limiter = False
-                    message = output[1].encode('ascii','ignore')
-                    channel.send_message("{}".format(message))
+                    message = output[1].encode('ascii', 'ignore')
+                    channel.send_message('{}'.format(message))
                     limiter = True
+
     def crons(self):
         for plugin in self.bot_plugins:
             plugin.do_jobs()
+
     def load_plugins(self):
-        for plugin in glob.glob(directory+'/plugins/*'):
+        for plugin in glob.glob(directory + '/plugins/*'):
             sys.path.insert(0, plugin)
-            sys.path.insert(0, directory+'/plugins/')
-        for plugin in glob.glob(directory+'/plugins/*.py') + glob.glob(directory+'/plugins/*/*.py'):
+            sys.path.insert(0, directory + '/plugins/')
+        for plugin in glob.glob(directory + '/plugins/*.py') + glob.glob(directory + '/plugins/*/*.py'):
             logging.info(plugin)
             name = plugin.split('/')[-1][:-3]
-#            try:
             self.bot_plugins.append(Plugin(name))
-#            except:
-#                print "error loading plugin %s" % name
+
 
 class Plugin(object):
-    def __init__(self, name, plugin_config={}):
+    def __init__(self, name, plugin_config=None):
+        plugin_config = plugin_config if plugin_config else {} # this isn't actually used...?
         self.name = name
         self.jobs = []
         self.module = __import__(name)
         self.register_jobs()
         self.outputs = []
         if name in config:
-            logging.info("config found for: " + name)
+            logging.info('config found for: ' + name)
             self.module.config = config[name]
         if 'setup' in dir(self.module):
             self.module.setup()
+
     def register_jobs(self):
         if 'crontable' in dir(self.module):
             for interval, function in self.module.crontable:
-                self.jobs.append(Job(interval, eval("self.module."+function)))
+                self.jobs.append(Job(interval, eval('self.module.' + function)))
             logging.info(self.module.crontable)
             self.module.crontable = []
         else:
             self.module.crontable = []
+
     def do(self, function_name, data):
         if function_name in dir(self.module):
-            #this makes the plugin fail with stack trace in debug mode
+            # this makes the plugin fail with stack trace in debug mode
             if not debug:
                 try:
-                    eval("self.module."+function_name)(data)
+                    eval('self.module.' + function_name)(data)
                 except:
-                    dbg("problem in module {} {}".format(function_name, data))
+                    dbg('problem in module {} {}'.format(function_name, data))
             else:
-                eval("self.module."+function_name)(data)
-        if "catch_all" in dir(self.module):
+                eval('self.module.' + function_name)(data)
+        if 'catch_all' in dir(self.module):
             try:
                 self.module.catch_all(data)
             except:
-                dbg("problem in catch all")
+                dbg('problem in catch all')
+
     def do_jobs(self):
         for job in self.jobs:
             job.check()
+
     def do_output(self):
         output = []
         while True:
             if 'outputs' in dir(self.module):
                 if len(self.module.outputs) > 0:
-                    logging.info("output from {}".format(self.module))
+                    logging.info('output from {}'.format(self.module))
                     output.append(self.module.outputs.pop(0))
                 else:
                     break
@@ -129,34 +141,39 @@ class Plugin(object):
                 self.module.outputs = []
         return output
 
+
 class Job(object):
     def __init__(self, interval, function):
         self.function = function
         self.interval = interval
         self.lastrun = 0
+
     def __str__(self):
-        return "{} {} {}".format(self.function, self.interval, self.lastrun)
+        return '{} {} {}'.format(self.function, self.interval, self.lastrun)
+
     def __repr__(self):
         return self.__str__()
+
     def check(self):
         if self.lastrun + self.interval < time.time():
             if not debug:
                 try:
                     self.function()
                 except:
-                    dbg("problem")
+                    dbg('problem')
             else:
                 self.function()
             self.lastrun = time.time()
             pass
+
 
 class UnknownChannel(Exception):
     pass
 
 
 def main_loop():
-    if "LOGFILE" in config:
-        logging.basicConfig(filename=config["LOGFILE"], level=logging.INFO, format='%(asctime)s %(message)s')
+    if 'LOGFILE' in config:
+        logging.basicConfig(filename=config['LOGFILE'], level=logging.INFO, format='%(asctime)s %(message)s')
     logging.info(directory)
     try:
         bot.start()
@@ -168,34 +185,25 @@ def main_loop():
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument(
-        '-c',
-        '--config',
-        help='Full path to config file.',
-        metavar='path'
-    )
+    parser.add_argument('-c', '--config', help='Full path to config file.', metavar='path')
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parse_args()
     directory = os.path.dirname(sys.argv[0])
     if not directory.startswith('/'):
-        directory = os.path.abspath("{}/{}".format(os.getcwd(),
-                                directory
-                                ))
+        directory = os.path.abspath('{}/{}'.format(os.getcwd(), directory))
 
     config = yaml.load(file(args.config or 'rtmbot.conf', 'r'))
-    debug = config["DEBUG"]
-    bot = RtmBot(config["SLACK_TOKEN"])
+    debug = config['DEBUG']
+    bot = RtmBot(config['SLACK_TOKEN'])
     site_plugins = []
     files_currently_downloading = []
     job_hash = {}
 
-    if config.has_key("DAEMON"):
-        if config["DAEMON"]:
-            import daemon
-            with daemon.DaemonContext():
-                main_loop()
+    if config.get('DAEMON'):
+        import daemon
+        with daemon.DaemonContext():
+            main_loop()
     main_loop()
-
